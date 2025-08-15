a32f2c6f197d2f428903eba8a558ac6640a1ef72
Valentina <yunnicova@gmail.com>
2025-08-15 21:30:12 +0200

    feat: button tabs.js

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -3,23 +3,67 @@
 const rootSelector = '[data-js-tabs]'; //блок "blog-title"
 
 class Tabs {
+  //поле свойства с css селекторами
+  selectors = {
+    root: rootSelector, //главный контейнер табов
+    button: '[data-js-tabs-button]', //строка с селектором для поиска кнопок вкладок.
+    content: '[data-js-tabs-content]' //строка с селектором для поиска контента вкладок.
+  };
+
   //поле
-selectors = {
-  root: rootSelector,
-  button: '[data-js-tabs-button]',
-  content: '[data-js-tabs-content]',
-} 
+  stateClasses = {
+    isActive: 'is-active' //css класс состояния
+  };
 
-//поле
-stateClasses = {
-  isActive: 'is-active', //css класс состояния
-}
+  //поле для обновления
+  stateAttributes = {
+    ariaSelected: 'aria-selected', //HTML-атрибута, который будет менять код.
+    tabIndex: tabindex //указывающее порядок фокуса при Tab-навигации
+  };
 
-//поле
-stateAttributes = {
-  ariaSelected: 'aria-selected',
-  tabIndex: tabindex,
-}
+  constructor(rootElement) {
+    this.rootElement = rootElement;
+    this.buttonElements = this.rootElement.querySelectorAll(
+      this.selectors.button
+    );
+    this.contentElements = this.rootElement.querySelectorAll(
+      this.selectors.content
+    );
+    this.state = {
+      activeTabIndex: [...this.buttonElements].findIndex((buttonElement) =>
+        buttonElement.classList.contains(this.stateClasses.isActive)
+      )
+    };
+    this.limitTabsIndex = this.buttonElements.length - 1; //лимит индекса табов
+    this.bindEvents();
+  }
+
+  updateUI() {
+    const { activeTabIndex } = this.state;
+
+    this.buttonElements.forEach((buttonElement, index) => {
+      const isActive = index === activeTabIndex;
+
+      buttonElement.classList.toggle(this.stateClasses.isActive, isActive);
+    });
+
+    this.contentElements.forEach((contentElement, index) => {
+      const isActive = index === activeTabIndex;
+
+      contentElement.classList.toggle(this.stateClasses.isActive, isActive);
+    });
+  }
+
+  onButtonClick(buttonIndex) {
+    this.state.activeTabIndex = buttonIndex;
+    this.updateUI();
+  }
+
+  bindEvents() {
+    this.buttonElements.forEach((buttonElement, index) => {
+      buttonElement.addEventListener('click', () => this.onButtonClick(index));
+    });
+  }
 }
 
 // для инициализации логики для всех табов на одной странице
@@ -30,10 +74,11 @@ class TabsCollection {
     this.init();
   }
 
+  //Находим все элементы в документе, подходящие под селектор rootSelector
   init() {
     document.querySelectorAll(rootSelector).forEach((element) => {
-      new Tabs(element);
-    }); //ищем все дом элементы
+      new Tabs(element); //Внутри цикла создаём новый экземпляр класса Tabs:
+    });
   }
 }
 
